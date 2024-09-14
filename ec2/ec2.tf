b3a9c39a6e7074713ac9a32b60fd634382506fd8
Manoharreddy1481 <pullagurlamanoharreddy@gmail.com>
2024-09-14 13:31:21 +0530

    terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -22,3 +22,13 @@ resource "aws_security_group" "allow_ssh_terraform" {
       Name = " allow_sshh " 
     }
 }
+
+resource "aws_instance" "terraform" {
+    ami="ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id] 
+
+    tags={
+        name="Terraform"
+    }
+}
